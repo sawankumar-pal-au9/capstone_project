1bd4929fed08693b01606c145bad716f1df9d94a
sawankumar-pal-au9 <67586262+sawankumar-pal-au9@users.noreply.github.com>
2021-03-25 12:29:48 +0530

    app till 25-03-21

--- view/client/src/setupProxy.js
+++ view/client/src/setupProxy.js
@@ -3,4 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware('/auth/google', {target: 'http://localhost:9800'}));
     app.use(createProxyMiddleware('/auth/facebook', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/categories/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/products/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/details/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/reviews/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/orders/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/cart/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/users/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/subcategories/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/coupons/*', {target: 'http://localhost:9800'}));
+    app.use(createProxyMiddleware('/contacts/*', {target: 'http://localhost:9800'}));
 };
